e82851401e67c461e4d552c008d1dd54c6974e0d
Pietari Virtanen <pietari.virtanen@edu.karelia.fi>
2021-03-14 01:35:56 +0200

    things 2

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/"> 
+    <Router basename="/webmob"> 
     <App />
     </Router>
   </React.StrictMode>,
